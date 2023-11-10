1ac524ee71e3aee85435fbf7dd1464b7acad13df
Do Cuong <hoangtuoxich@gmail.com>
2023-11-10 20:50:33 +0700

    data

---
+++ BTL/Data/LaptopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using LaptopShop.Models.EF;
+using LaptopShop.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaptopShop.Data
+{
+    public class LaptopDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public LaptopDbContext(DbContextOptions<LaptopDbContext> options) : base(options) { }
+
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
